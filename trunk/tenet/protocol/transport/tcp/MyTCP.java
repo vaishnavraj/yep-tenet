225aeb9a7eca58b0bf76afc45851de0e51c66d51
yepyao <yepyao@d0c62af3-0b2e-aed2-c79a-dcc03750bde8>
2011-12-15 10:19:03 +0000

    a new version of TCP can run but wrong

--- trunk/tenet/protocol/transport/tcp/MyTCP.java
+++ trunk/tenet/protocol/transport/tcp/MyTCP.java
@@ -14,6 +14,7 @@ import tenet.protocol.datalink.IDataLinkLayer;
 import tenet.protocol.interrupt.InterruptObject;
 import tenet.protocol.interrupt.InterruptParam;
 import tenet.protocol.network.ipv4.IPProtocol;
+import tenet.protocol.network.ipv4.IPReceiveParam;
 import tenet.protocol.transport.tcp.TCPProtocol.ReturnStatus;
 import tenet.protocol.transport.tcp.TCPProtocol.ReturnType;
 import tenet.util.ByteLib;
@@ -29,6 +30,14 @@ public class MyTCP extends InterruptObject implements TCPProtocol {
 	private LinkedList<IClient<Integer>> m_app_layers = new LinkedList<IClient<Integer>>();
 	
 	public static final Integer protocolID = 0x06;
+	
+	public void P(String s){
+		System.out.println(s);
+	}
+	
+	public MyTCP(){
+		P("new TCP");
+	}
 	
 	@Override
 	public void disable() {
@@ -73,6 +82,7 @@ public class MyTCP extends InterruptObject implements TCPProtocol {
 	@Override
 	public void registryClient(IClient<Integer> client) {
 		m_app_layers.add(client);
+		P("registryClient");
 		client.attachTo(this);
 	}
 
@@ -129,6 +139,7 @@ public class MyTCP extends InterruptObject implements TCPProtocol {
 	
 	@Override
 	public int socket() {
+		P("socket"+nextHandle);
 		MySocket socket = new MySocket(nextHandle,this);
 		handleSocket.put(nextHandle, socket);
 		nextHandle++;
@@ -138,60 +149,101 @@ public class MyTCP extends InterruptObject implements TCPProtocol {
 
 	@Override
 	public int bind(int handle, int port) {
+		P("bind"+handle+" "+port);
 		if (portHandle.containsKey(port)) return -1;
 		portHandle.put(port, handleSocket.get(handle));
-		handleSocket.get(port).bind(ByteLib.bytesToInt(m_node.getAddress(m_IP), 0), port);
+		
+		handleSocket.get(handle).bind(ByteLib.bytesToInt(m_node.getAddress(m_IP), 0), port);
+		P("bind end");
 		return 0;
 	}
 
 	@Override
 	public void listen(int handle) {
+		P("listen");
 		//check in the postHandle
 		handleSocket.get(handle).Listen();
-
+		P("listen end");
 	}
 
 	@Override
 	public void connect(int handle, int dstIP, int dstPort) {
+		P("Connect IP:"+dstIP+" Port:"+dstPort);
 		//check in the postHandle
+		for (int i=10000;i<0xFFFF;i++){
+			if (bind(handle, i)==0) break;
+		}
 		handleSocket.get(handle).Connect(dstIP, dstPort);
 
 	}
 
 	@Override
 	public void close(int handle) {
+		P("close");
 		handleSocket.get(handle).close();
 
 	}
 
 	@Override
 	public void accept(int handle) {
-		// TODO Auto-generated method stub
+		P("accept");
 
 	}
 
 	@Override
 	public void abort(int handle) {
+		P("abort");
 		handleSocket.get(handle).abort();
 
 	}
 
 	@Override
 	public void send(int handle, byte[] data) {
+		P("send");
 		handleSocket.get(handle).send(data);
 
 	}
 
 	@Override
 	public void receive(int handle) {
+		P("receive");
 		handleSocket.get(handle).receive();
 
 	}
 
 	@Override
 	protected void interruptHandle(int signal, InterruptParam param) {
-		// TODO Auto-generated method stub
+		if(signal==IPProtocol.recPacketSignal){
+			P("seg arrived");
+			if (param instanceof IPReceiveParam){
+				IPReceiveParam pkt = (IPReceiveParam)param;
+				Integer srcip = pkt.getSource();
+				Integer destip = pkt.getDestination();
+				TCPSegment recv = new TCPSegment(pkt.getData());
+				int srcport = recv.SourcePort;
+				int destport = recv.DestinationPort;
+				MySocket socket = getRightSocket(srcip, srcport, destip, destport);
+				P("socket find");
+				socket.segmentArrives(recv, srcip);
+			}
+		}
 
+	}
+	
+	private MySocket getRightSocket(Integer srcip, int srcport, Integer destip, int destport){
+		for (Integer handle : handleSocket.keySet()){
+			MySocket skt = handleSocket.get(handle);
+			if (skt.dest_ip!=null && skt.src_ip.equals(destip) && skt.dest_ip.equals(srcip) && skt.src_port == destport && skt.dest_port == srcport){
+				return skt;
+			}
+		}
+		for (Integer port : portHandle.keySet()){
+			MySocket skt = portHandle.get(port);
+			if (skt.src_ip.equals(destip) && skt.src_port == destport){
+				return skt;
+			}
+		}
+		return idleSocket;
 	}
 	
 	public void ReturnMsg(ReturnType type, int handle,	ReturnStatus state, int result, byte[] data) {
@@ -207,6 +259,7 @@ public class MyTCP extends InterruptObject implements TCPProtocol {
 	}
 	
 	public void sendSeg(TCPSegment seg, Integer src_ip, Integer dest_ip) {
+		P("send Seg "+src_ip+" "+dest_ip);
 		m_IP.sendPacket(seg.toBytes(), src_ip, dest_ip, this.getUniqueID());
 	}
 
